8896de84dc8bb885d8fb823a6a3c64d5187f4f2b
jayln <122861938+jaylnxyz@users.noreply.github.com>
2023-04-29 19:36:41 -0700

    Delete index.js

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const date = new Date();
-const time = date.toLocaleDateString("default", {
-    month: "long",
-    day: "numeric",
-    year: "numeric"
-}).toLowerCase()
-
-document.getElementById("time").innerHTML = `${time}, today is a new day.`
